8d42793893195b5df51b0f2f529052cfee606202
Santi Izquierdo <santyizquierdo@gmail.com>
2025-10-06 19:13:40 +0000

    Ejercicio completado

--- src/app.py
+++ src/app.py
@@ -1,10 +1,33 @@
 import os
 import pandas as pd
-import matplotlib.pyplot as plt
+import seaborn as sns
 from dotenv import load_dotenv
 
-# load the .env file variables
+
+import os
+from dotenv import load_dotenv
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
 load_dotenv()
 
 
-# Spotify API credentials
+client_id = os.getenv("CLIENT_ID")
+client_secret = os.getenv("CLIENT_SECRET")
+
+
+auth_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+spotify = spotipy.Spotify(auth_manager=auth_manager)
+
+
+artist_uri = 'spotify:artist:6XyY86QOPPrYVGvF9ch6wz'
+
+
+results = spotify.artist_top_tracks(artist_uri)
+
+
+for track in results['tracks'][:10]:
+    print('🎵 Track    :', track['name'])
+    print('🔗 Audio    :', track['preview_url'])
+    print('🖼️ Cover art:', track['album']['images'][0]['url'])
+    print()
